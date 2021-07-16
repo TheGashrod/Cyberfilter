2460ded9990dd68eb10a54a82dd01f0d5be1ef67
simo <simoelmawork@gmail.com>
2021-07-16 17:29:22 +0200

    byPass tools

--- Cyber filter/src/application/Main.java
+++ Cyber filter/src/application/Main.java
@@ -1,8 +1,10 @@
 /**
- * 
+ * https://github.com/dnsjava/dnsjava  (for dns part)
+ * https://www.pandasecurity.com/fr/mediacenter/technologie/proxy-et-comment-utiliser/   (how to use a proxy)
  */
 package application;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
@@ -19,26 +21,38 @@ import tools.Tools;
 public class Main {
 	
 
-	public static void main(String[] args) throws MalformedURLException, UnknownHostException	{
+	public static void main(String[] args) throws IOException	{
 		URL url1 = new URL("http://www.Orange.com/test/stage/orange");
 		URL url2 = new URL("http://www.badInternetconnection.com/test/Internship");
 		
-		// Test de la décomposition d'une url             ---> validé
+//		Test de la décomposition d'une url                                 ---> V
 		ArrayList<String> decomposedUrl1 = new ArrayList<String>();
 		decomposedUrl1 = Tools.urlDecomposition(url1);
 		System.out.println(decomposedUrl1);
 		
-		// test du stockage des données la base de donnée par MsIsdn           --->
+//      Test du stockage des données la base de donnée par MsIsdn          ---> V
 		MsIsdn msIsdn1 = new MsIsdn("0612345678");
 		DataBase.addDataForMsIsdn(msIsdn1, decomposedUrl1);
 		
-//		MsIsdn msIsdn2 = new MsIsdn("0687654321");
+		//MsIsdn msIsdn2 = new MsIsdn("0687654321");
 		ArrayList<String> decomposedUrl2 = new ArrayList<String>();
 		decomposedUrl2 = Tools.urlDecomposition(url2);
 		DataBase.addDataForMsIsdn(msIsdn1, decomposedUrl2);
 		
 		System.out.println(DataBase.getdBbyMsIsdn());
 		
+//		Test de la fonction byPass                                         ---> V
+//		System.out.println(Tools.byPass());
+		
+//		Test de l'ajout de -bypass au nom de domaine                       ---> V
+		String byPassDelimiter = "-slqfkdz###@@@@@"; 
+		System.out.println(Tools.addByPassStringToDomainName(decomposedUrl1.get(1), byPassDelimiter));
+		System.out.println(Tools.addByPassStringToDomainName(decomposedUrl2.get(1), byPassDelimiter));
+		
+		
+//      Test le fait de reperer -bypass dans un nom de domain
+		String newDomainName = Tools.addByPassStringToDomainName(decomposedUrl1.get(1), byPassDelimiter);
+		System.out.println(Tools.containsByPass(newDomainName, byPassDelimiter));
 	}
 
 }
--- Cyber filter/src/tools/Tools.java
+++ Cyber filter/src/tools/Tools.java
@@ -1,11 +1,18 @@
 package tools;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public abstract class Tools {
+
+	
+	
+//#################################### FUNCTIONS LINKED TO THE URL : ###########################################################""""
 	
 	/**
 	 * @param url
@@ -45,7 +52,6 @@ public abstract class Tools {
 	}
 	
 	
-	
 	/**
 	 * @param url
 	 * @return @IP of a domain name
@@ -65,5 +71,75 @@ public abstract class Tools {
 	public static String domainNameOfUrl(URL url) {
 		return url.getHost();
 	}
+	
+	
+
+	
+//#################################### FUNCTIONS LINKED TO THE BYPASS : ###########################################################""""
+	
+	/**
+	 * @return true if hhe user wants to bypass, false else
+	 * @throws IOException
+	 */
+	public static boolean byPass() throws IOException {
+		System.out.println("This is a dangerous link, tape: yes, if you still want to open it.");
+		 // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+         // Reading data using readLine
+        String input = reader.readLine();
+        if(input.equals("yes")) {
+        	return true;
+        }
+        else return false;
+	}
+	
+	
+	/**
+	 * @param domainName
+	 * @param byPassDelimiter : the string that we wanna add to the domainName
+	 * @return The domainName with -bypass in it
+	 */
+	public static String addByPassStringToDomainName(String domainName, String byPassDelimiter) {
+		int n = domainName.length();
+		String newDomainName = "";
+		int i = domainName.length()-1;
+		while (domainName.charAt(i) != '.' && i>0) {
+			i-=1;
+		}
+		i-=1;
+		int b = n-(i+1);
+		for (int j = 0; j<=i; j++) {
+			newDomainName += domainName.charAt(j);
+		}
+		newDomainName += byPassDelimiter;
+		while(b>0) {
+			newDomainName += domainName.charAt(i+1);
+			i+=1;
+			b-=1;
+		}
+		return newDomainName;
+	}
+	
+	
+	public static boolean containsByPass(String domainName, String byPassDelimiter) {
+		int m = byPassDelimiter.length();
+		int i = domainName.length()-1;
+		while (domainName.charAt(i) != '-' && i>0) {
+			i-=1;
+		}
+		i-=1;
+		String testByPassDelimiter = "";
+		for (int j = 0; j<m; j++) {
+			testByPassDelimiter += domainName.charAt(i+1);
+			i+=1;
+		}
+		System.out.println(testByPassDelimiter);
+		if (testByPassDelimiter.equals(byPassDelimiter)) {
+			return true;
+		}
+		else return false;
+	}
+	
 
 }
