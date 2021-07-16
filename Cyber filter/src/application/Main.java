94de38fa7482bf7590c23dbe6d079ffe6145359d
simo <simoelmawork@gmail.com>
2021-07-16 11:32:58 +0200

    first commit

---
+++ Cyber filter/src/application/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package application;
+
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+
+/**
+ * @author dell
+ *
+ */
+public class Main {
+	
+
+	public static void main(String[] args)	{
+		
+		System.out.println("Hello");
+		System.out.println("test");
+	}
+
+}
